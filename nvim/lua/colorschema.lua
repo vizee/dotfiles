055d5c8939186309957622dd9854c2db02ed7ee0
vizee <vizee@live.cn>
2022-02-26 20:53:54 +0800

    update colorscheme

--- nvim/lua/colorschema.lua
+++ nvim/lua/colorschema.lua
@@ -1,9 +1,9 @@
-vim.g.quantum_black = 1
-vim.g.quantum_italics = 0
+-- vim.g.quantum_black = 1
+-- vim.g.quantum_italics = 0
 
 vim.cmd [[
 try
-  colorscheme quantum
+  colorscheme onedark
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme desert
 endtry
