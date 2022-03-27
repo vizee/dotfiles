b512e6f38ab3e9f1f0e63da9514411ba689a6f9f
vizee <vizee@live.cn>
2022-03-27 22:24:06 +0800

    update lsp.lua

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -9,8 +9,9 @@ vim.diagnostic.config({
 
 -- nvim-cmp & luasnip
 local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    -- local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    -- return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    return false
  end
 
 local luasnip = require("luasnip")
