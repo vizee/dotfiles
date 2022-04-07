70728e0935dc85b54fdbff147f9d569d953c97f1
vizee <vizee@live.cn>
2022-04-08 00:55:42 +0800

    add rust configs

--- rust-analyzer/update.sh
+++ rust-analyzer/update.sh
@@ -7,8 +7,14 @@ if [[ -f "./version.txt" ]]; then
     CUR=$(cat ./version.txt)
 fi
 
-if [ -z "$TAG" ]; then
-TAG=$(date -dlast-monday +%Y-%m-%d)
+uname=$(uname)
+
+if [[ -z "$TAG" ]]; then
+    if [[ "$uname" = 'Darwin' ]]; then
+        TAG=$(date -v-monday +%Y-%m-%d)
+    else
+        TAG=$(date -dlast-monday +%Y-%m-%d)
+    fi
 fi
 
 echo "current version: $CUR"
@@ -17,25 +23,32 @@ if [[ "$CUR" = "$TAG" ]]; then
     exit 0
 fi
 
-echo "upgrade to $TAG"
+echo "upgrading to $TAG"
 
-GZFILE=rust-analyzer-x86_64-unknown-linux-gnu.gz
+if [[ -z "$DLNAME" ]]; then
+    if [[ "$uname" = 'Darwin' ]]; then
+        DLNAME=rust-analyzer-x86_64-apple-darwin
+    else
+        DLNAME=rust-analyzer-x86_64-unknown-linux-gnu
+    fi
+fi
+
+GZFILE=$DLNAME.gz
+RABIN=rust-analyzer
 
 if [[ ! -f ./$GZFILE ]]; then
-    [[ -z "$TAG" ]] && echo "TAG not set" && exit 1
-
     TEMP=/tmp/$GZFILE
-    wget -O $TEMP "https://github.com/rust-analyzer/rust-analyzer/releases/download/$TAG/$GZFILE"
+    wget $WGETARGS -O $TEMP "https://github.com/rust-analyzer/rust-analyzer/releases/download/$TAG/$GZFILE"
     mv $TEMP ./$GZFILE
 fi
 
 gzip -d -f $GZFILE
-if [[ -f ./rust-analyzer-linux ]]; then
-    mv ./rust-analyzer-linux ./rust-analyzer-linux-old
-    echo -e "\033[32mold version: $(./rust-analyzer-linux-old --version || true)\033[0m"
+if [[ -f ./$RABIN ]]; then
+    mv ./$RABIN ./$RABIN-old
+    echo -e "\033[32mold version: $(./$RABIN-old --version || true)\033[0m"
 fi
 
-mv ./rust-analyzer-x86_64-unknown-linux-gnu ./rust-analyzer-linux
-chmod a+x ./rust-analyzer-linux
+mv ./$DLNAME ./$RABIN
+chmod a+x ./$RABIN
 echo $TAG > ./version.txt
-echo -e "\033[32mupgrade to version $TAG ($(./rust-analyzer-linux --version))\033[0m"
+echo -e "\033[32mupgrade to version $TAG ($(./$RABIN --version))\033[0m"
