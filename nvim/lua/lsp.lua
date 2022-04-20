6705e38655715577c9a1c9f8b2c052e33dae0aec
vizee <vizee@live.cn>
2022-04-20 12:42:20 +0800

    update nvim

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -43,8 +43,8 @@ cmp.setup({
                 fallback()
             end
         end, { "i", "s" }),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<Up>'] = cmp.mapping.select_prev_item(),
+        ['<Down>'] = cmp.mapping.select_next_item(),
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
