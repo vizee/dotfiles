c529f7089596308a0eb82f585fd980f229db07bc
vizee <vizee@live.cn>
2023-07-05 21:34:46 +0800

    update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,13 @@
 require 'options'
+
+if not vim.g.vscode then
 require 'plugins'
 require 'keybindings'
 require 'colorschema'
 require 'customize'
 require 'lsp'
 require 'snippets'
+end
 
 if vim.g.neovide then
     require 'neovide'
