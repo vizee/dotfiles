af82be9400a41e0305d9bbfb9ccbcb4286de4cfa
vizee <vizee@live.cn>
2024-07-30 00:39:23 +0800

    update nvim

--- nvim/lua/customize.lua
+++ nvim/lua/customize.lua
@@ -76,6 +76,7 @@ require'nvim-tree'.setup {
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {'c', 'go', 'rust'},
     sync_install = false,
+    auto_install = false,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
