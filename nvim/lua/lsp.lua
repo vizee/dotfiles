b29b3bd7b90d826fae883444cc832fc09d67e471
vizee <vizee@live.cn>
2025-04-24 00:31:27 +0800

    feat: update nvim

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -171,13 +171,14 @@ setup_rust()
 -- go
 
 local _organize_go_imports = function(wait_ms)
-    local params = vim.lsp.util.make_range_params()
+    local offset_encoding = client and client.offset_encoding or 'utf-8'
+    local params = vim.lsp.util.make_range_params(0, offset_encoding)
     params.context = {only = {"source.organizeImports"}}
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
     for _, res in pairs(result or {}) do
         for _, r in pairs(res.result or {}) do
             if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+                vim.lsp.util.apply_workspace_edit(r.edit, offset_encoding)
             else
                 vim.lsp.buf.execute_command(r.command)
             end
